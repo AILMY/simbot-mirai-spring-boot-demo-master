8e60c78da58cc9b5d47d70719a03afd2c2c519f0
ailm <979986531@qq.com>
2022-05-24 16:47:36 +0800

    空格

--- src/main/java/simbot/example/task/RunTimeAddSchedule.java
+++ src/main/java/simbot/example/task/RunTimeAddSchedule.java
@@ -2,6 +2,7 @@ package simbot.example.task;
 
 import lombok.extern.slf4j.Slf4j;
 import love.forte.simbot.bot.BotManager;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -14,32 +15,34 @@ import java.util.Date;
 @Component
 public class RunTimeAddSchedule implements SchedulingConfigurer {
     private final ThreadPoolTaskScheduler mScheduler = new ThreadPoolTaskScheduler(); //可以动态往里面添加定时任务
-    private BotManager botManager;
+    private final BotManager botManager;
     @Autowired
     public RunTimeAddSchedule(BotManager botManager) {
         this.botManager = botManager;
     }
 
     @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    public void configureTasks(@NotNull ScheduledTaskRegistrar taskRegistrar) {
         mScheduler.initialize();
     }
 
     public void setScheduler(Date date,YiMiao yiMiao) {
+
         Runnable run = new Runnable() {
             private YiMiao yiMiao;
             public Runnable setYiMiao(YiMiao yiMiao) {
                 this.yiMiao = yiMiao;
                 return this;
             }
+
             @Override
             public void run() {
-                //TODO:JOB
                 botManager.getDefaultBot().getSender().SENDER.sendGroupMsg(419663585,"即将开始"+yiMiao.toString());
             }
+
         }.setYiMiao(yiMiao);
+
         mScheduler.schedule(run,date);
         log.info("增加定时任务:\ntime:"+date+"\n对象:"+yiMiao.toString());
     }
-
 }
